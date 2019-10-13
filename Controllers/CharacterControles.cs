7c5c8a7c670c5762f2f6061f577ec1fba862990c
0111001101110011 <42249056+0111001101110011@users.noreply.github.com>
2019-10-13 12:18:37 -0700

    mas cambios

--- Controllers/CharacterControles.cs
+++ Controllers/CharacterControles.cs
@@ -1,64 +1,57 @@
-  
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
-using web_api_simpsons.Modules;
 using web_api_simpsons.Dependencies;
+using System.Collections.Generic;
+using web_api_simpsons.Modules; 
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
 
 namespace web_api_simpsons.Controllers
 {
     [Route("simpsons/[controller]")]
     [ApiController]
+    [EnableCors("AllowOrigin")]
+
     public class CharacterController : ICharacter
     {
-        List<Character> listOfCharacters => new List<Character>
-    {
-        new Character
+        List<Character> characterList => new List<Character>
         {
-            FirstName = "Homero",
+            new Character
+            {
+                FirstName = "Homero",
                 SecondName = "Jay",
-                LastName = "Simpsons",
+                LastName = "Simpson",
                 Age = 34,
-            }
+                Description = "Esposo de Marge y padre de Bart, Lisa y Maggie."
+            },
             new Character
             {
-                FirstName = "Marge ",
-                SecondName = " ",
-                LastName = "Simpsons",
-                Age = 36,
-            }
+                FirstName = "Bartolomeo",
+                SecondName = "Jay",
+                LastName = "Simpson",
+                Age = 10,
+            },
             new Character
             {
-                FirstName = "Lisa ",
-                SecondName = " ",
-                LastName = "Simpsons",
+                FirstName = "Margory",
+                LastName = "Simpson",
+                Age = 34,
+            },
+            new Character
+            {
+                FirstName = "Lisa",
+                LastName = "Simpson",
                 Age = 8,
             }
-            new Character
-            {
-                FirstName = "Bart ",
-                SecondName = " ",
-                LastName = "Simpsons",
-                Age = 10,
-            }
-            new Character
-            {
-                FirstName = "Maggie ",
-                SecondName = " ",
-                LastName = "Simpsons",
-                Age = 1,
-            }
         };
-        
-        [HttpGet("{id}")]
-        public Character GetCharacter(int id)
-        {
-            return listOfCharacters[id];
-        }
 
         [HttpGet]
         public List<Character> GetCharacterList()
         {
-            return listOfCharacters;
+            return characterList;
         }
-    }
-}
+
+        [HttpGet("{id}")]
+        public Character GetCharacter(int id)
+        {
+            return characterList[id];
+        }
+    }}
